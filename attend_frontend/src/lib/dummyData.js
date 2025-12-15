0df10fa8d85fd1eb9529332af17fcf52e839aea7
rijanniraula <rijann38@gmail.com>
2025-12-15 20:00:54 +0545

    dummy data

---
+++ attend_frontend/src/lib/dummyData.js
@@ -0,0 +1,54 @@
+export const dummyDeviceInfo = {
+  success: true,
+  userCounts: 38,
+  logCounts: 7641,
+  logCapacity: 200000,
+  deviceName: "ZKTECO Device",
+};
+
+export const dummyAttendanceLogs = {
+  success: true,
+  data: [
+    {
+      sn: 1,
+      user_id: "1",
+      record_time: "Thu Apr 17 2025 18:17:10 GMT+0545 (Nepal Time)",
+      type: 1,
+      state: 0,
+      ip: "192.168.1.6",
+    },
+    {
+      sn: 2,
+      user_id: "2",
+      record_time: "Fri Apr 18 2025 10:41:00 GMT+0545 (Nepal Time)",
+      type: 1,
+      state: 0,
+      ip: "192.168.1.6",
+    },
+    {
+      sn: 3,
+      user_id: "3",
+      record_time: "Fri Apr 18 2025 09:15:22 GMT+0545 (Nepal Time)",
+      type: 1,
+      state: 0,
+      ip: "192.168.1.6",
+    },
+    {
+      sn: 4,
+      user_id: "1",
+      record_time: "Fri Apr 18 2025 17:30:45 GMT+0545 (Nepal Time)",
+      type: 0,
+      state: 1,
+      ip: "192.168.1.6",
+    },
+    {
+      sn: 5,
+      user_id: "4",
+      record_time: "Mon Apr 21 2025 08:45:10 GMT+0545 (Nepal Time)",
+      type: 1,
+      state: 0,
+      ip: "192.168.1.6",
+    },
+  ],
+  totalLogs: 8707,
+};
